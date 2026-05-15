07d74bab3b3644f4f73a97195fe633c020eb1e55
Abdulmalk Alnajem <your_id@ksu.edu.sa>
2026-05-15 17:16:18 +0300

    Organize repo: Added project folders and professional READMEs

--- HoursCounter.py
+++
@@ -1,5 +0,0 @@
-seconds = int(input("Enter the duration in seconds: "))
-hours = seconds // 3600
-minutes = (seconds % 3600) // 60
-remaining_seconds = seconds % 60
-print(f"The duration is: {hours} hours, {minutes} minutes, {remaining_seconds} seconds")
